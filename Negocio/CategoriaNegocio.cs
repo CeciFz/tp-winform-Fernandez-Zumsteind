476a5add1d399284083a74431545e4f034368185
zumsteind <zumsteind@gmail.com>
2022-05-03 21:10:57 -0300

    se agrego class categoria negocio

--- Negocio/CategoriaNegocio.cs
+++ Negocio/CategoriaNegocio.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Dominio;
 namespace Negocio
 {
     public class CategoriaNegocio
     {
+
+        public List<Categoria> listarMarca()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("select id, descripcion from CATEGORIAS");
+                datos.lecturaDatos();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id = (int)datos.Lector["id"];
+
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+
     }
 }
